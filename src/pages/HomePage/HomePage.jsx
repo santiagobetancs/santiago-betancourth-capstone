fc98f333713246b57ac6cc51a411d12ff33180eb
santiagobetancs <160289567+santiagobetancs@users.noreply.github.com>
2024-06-26 14:03:15 -0400

    created intro page with styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import Header from '../../components/Header/Header';
 import MainBlock from '../../components/MainBlock/MainBlock';
 import './HomePage.scss';
+import book from '../../assets/images/book.svg'
 
 export default function HomePage() {
     return (
@@ -8,11 +9,31 @@ export default function HomePage() {
             <Header />
             <main className="home">
                 <MainBlock>
-
+                    <div className='home__block'>
+                        {/* <h2 className='home__intro'>
+                            Welcome
+                        </h2> */}
+                        {/* <h4 className='home__prompt'>
+                            Scroll down to find out
+                        </h4> */}
+                        <img className='home__book' src={book} alt="book" />
+                    </div>
                 </MainBlock>
-                <h1 data-aos="fade-left" className='home__title'>
-                    StoryTime
-                </h1>
+                <div data-aos="fade-up" className='home__scroll'>
+                    <MainBlock>
+                        <div className='home__block home__block--scroll'>
+                            <h2 className='home__text home__text--top'>
+                                Let us help you pick your next book
+                            </h2>
+                            <h2 className='home__text'>
+                                Log In
+                            </h2>
+                            <h2 className='home__text home__text--bottom'>
+                                Sign Up
+                            </h2>
+                        </div>
+                    </MainBlock>
+                </div>
             </main>
         </>
     )
