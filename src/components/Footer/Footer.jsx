b2f48881f21c7dbd57ceb6d83f229145dbb5c4ab
santiagobetancs <160289567+santiagobetancs@users.noreply.github.com>
2024-06-27 10:21:31 -0400

    added about us link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import './Footer.scss'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
     return (
         <>
             <main className="footer">
-                <h2 className="footer__title">
-                    About Us
-                </h2>
+                <Link className='footer__link' to='/aboutus'>
+                    <h2 className="footer__title">
+                        About Us
+                    </h2>
+                </Link>
             </main>
         </>
     )
