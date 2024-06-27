6f1685a2369b5e72b042bd76d51962598639bbc1
santiagobetancs <160289567+santiagobetancs@users.noreply.github.com>
2024-06-27 10:41:02 -0400

    started profile page

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import './ProfilePage.scss'
+import Header from '../../components/Header/Header'
+
+export default function ProfilePage() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
